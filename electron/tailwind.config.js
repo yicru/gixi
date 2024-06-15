ed67cb0cd8ba647de7543e3d062152f5f508a797
yuichiro miyamae <yu1ro.mya@gmail.com>
2024-06-15 13:33:01 +0900

    add: catalyst

---
+++ electron/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['./src/renderer/index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
